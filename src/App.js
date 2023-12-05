5bf5c7b43116af05ec67ef1e3216e5a90c6e648d
vivekmaru36 <vivekmaru036@gmail.com>
2023-12-05 20:46:24 +0530

    login done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
@@ -34,36 +33,3 @@ const App = () => {
 };
 
 export default App;
-=======
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
-import Signup from './Signup';
-import Registration from './Registration';
-import Navbar from './Navbar';
-import './App.css';
-import Dashboard from './Dashboard/Dashboard';
-import './Dashboard/Dashboard.css'; 
-
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/registration" element={<Registration />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
-};
-
-export default App;
->>>>>>> 615d3f24881930491b6da80b834ca16a6556b355
--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -112,49 +111,3 @@ const Login = () => {
 };
 
 export default Login;
-=======
-import React, { useState } from 'react';
-
-const Login = () => {
-  const [rfid, setRFID] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log('RFID:', rfid);
-    console.log('Password:', password);
-    // Implement your login logic here
-  };
-
-  return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="rfid">RFID:</label>
-          <input
-            type="text"
-            id="rfid"
-            value={rfid}
-            onChange={(e) => setRFID(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-};
-
-export default Login;
->>>>>>> 615d3f24881930491b6da80b834ca16a6556b355
